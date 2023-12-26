6ea5dc849ff67dc259d56e4c79e2ff07445a58df
Fangtnw <thanawat.smkn@egmail.com>
2023-12-26 12:25:41 +0700

    update

--- arduino/diffdrive/diffdrive_teleop/coop_teleop/coop_teleop.ino
+++ arduino/diffdrive/diffdrive_teleop/coop_teleop/coop_teleop.ino
@@ -1,6 +1,10 @@
 #include <PID_v1.h>
 #include <Xicro_coop_teleop_ID_1.h>
+#include <Wire.h>
 #include <Servo.h>
+#include <MPU6050_light.h>
+
+ MPU6050 mpu(Wire);
 Servo myservo; 
 Xicro xicro;
 
@@ -11,7 +15,6 @@ unsigned int noCommLoops = 0;                 //main loop without communication 
 
 double speed_cmd_left2 = 0;   
 unsigned long lastMilli = 0;
-
 
 // Motor 1
 const int motor1INA = 12;
@@ -47,6 +50,20 @@ double speed_act_right = 0;                   //Actual speed for right wheel in 
 double speed_cmd_right = 0;                   //Command speed for right wheel in m/s 
                         
 const double max_speed = 0.4;                 //Max speed in m/s
+
+double speedlinx = 0;
+double speedliny = 0;
+double speedangz = 0;                 //Desired angular speed for the robot, in rad/s
+float ax, ay, az;  
+float gx, gy, gz;  
+
+float orientation[4]={0};
+float orientation_covariance[9]={1,0,0,0,1,0,0,0,1};
+float angular_velocity[3]={0};
+float angular_velocity_covariance[9]={1,0,0,0,1,0,0,0,1};
+float linear_acceleration[3]={0};
+float linear_acceleration_covariance[9]={1,0,0,0,1,0,0,0,1};
+double theta = 0 ;
 
 int PWM_leftMotor = 0;                     //PWM command for left motor
 int PWM_rightMotor = 0;                    //PWM command for right motor 
@@ -103,10 +120,22 @@ void handle_cmd() {
   speed_req_left = speed_req - angular_speed_req*(wheelbase/2);     //Calculate the required speed for the left motor to comply with commanded linear and angular speeds
   speed_req_right = speed_req + angular_speed_req*(wheelbase/2);    //Calculate the required speed for the right motor to comply with commanded linear and angular speeds
 
-  xicro.Publisher_speedreq.message.vector.x = speed_req_left;
-  xicro.Publisher_speedreq.message.vector.y = speed_req_right;
-        
-  xicro.publish_speedreq();
+//  xicro.Publisher_speedreq.message.vector.x = speed_req_left;
+//  xicro.Publisher_speedreq.message.vector.y = speed_req_right;
+//        
+//  xicro.publish_speedreq();
+
+//    xicro.Publisher_speedcmd.message.vector.x = speed_cmd_left;
+//    xicro.Publisher_speedcmd.message.vector.y = speed_cmd_right;
+//          
+//    xicro.publish_speedcmd();
+//  
+//    xicro.Publisher_PWM.message.vector.x = PWM_leftMotor;
+//    xicro.Publisher_PWM.message.vector.y = PWM_rightMotor;
+//          
+//    xicro.publish_PWM();
+//
+
       
 }
 
@@ -114,6 +143,10 @@ motor leftMotor, rightMotor;
 
 void setup() {
   // Set the motor control pins as outputs
+  Wire.begin();
+  byte status = mpu.begin();
+  mpu.calcOffsets(); // gyro and accelero
+  
   pinMode(motor1INA, OUTPUT);
   pinMode(motor1INB, OUTPUT);
   pinMode(motor1PWM, OUTPUT);
@@ -128,8 +161,6 @@ void setup() {
   Serial.println("Setup complete");
   
   float linearX = xicro.Subscription_cmd_vel.message.linear.x;
-  Serial.print(linearX);
-  Serial.print("kuy");
 
   leftMotor.pinA = 12;
   leftMotor.pinB = 11;
@@ -173,6 +204,12 @@ void loop() {
   {                                                                           // enter timed loop
     lastMilli = millis();
     
+    byte status = mpu.begin();
+    
+    xicro.Publisher_IMU.message.vector.x = mpu.getAngleX();
+    xicro.Publisher_IMU.message.vector.y = mpu.getAngleY();
+    xicro.Publisher_IMU.message.vector.z = mpu.getAngleZ();
+//    xicro.Publisher_IMU.message.vector.y = int(mpu.getAngleZ())%360;
     
     if (abs(pos_left) < 5){                                                   //Avoid taking in account small disturbances
       speed_act_left = 0;
@@ -188,9 +225,19 @@ void loop() {
     speed_act_right=((pos_right/encoder_cpr)*2*PI)*(1000/LOOPTIME)*radius;          // calculate speed of right wheel
     }
     
+    xicro.Publisher_encoder_tick.message.vector.x = pos_right;
+    xicro.Publisher_encoder_tick.message.vector.y = pos_left;
     pos_left = 0;
     pos_right = 0;
-
+    
+//    theta = theta * 180 / 3.141592653589793238462643383279502 ;
+//    if(theta > 360)
+//    {
+//      theta = abs(theta - 360);
+//    }
+//    xicro.Publisher_encoder_tick.message.vector.z = theta;
+//    theta = theta * 3.141592653589793238462643383279502 / 180 ;
+    
     speed_cmd_left = constrain(speed_cmd_left, -max_speed, max_speed);
     PID_leftMotor.Compute();                                                 
     // compute PWM value for left motor. Check constant definition comments for more information.
@@ -244,45 +291,45 @@ void loop() {
       noCommLoops = noCommLoopMax;
     }
 
+   publishSpeed(LOOPTIME);
 
-//   speedcmd.vector.x = speed_cmd_left;    //left wheel speed (in m/s)
-//   speedcmd.vector.y = speed_cmd_right;   //right wheel speed (in m/s)
-//  speed_cmd.publish(&speedcmd);
-
-    xicro.Publisher_speedcmd.message.vector.x = speed_cmd_left;
-    xicro.Publisher_speedcmd.message.vector.y = speed_cmd_right;
-          
-    xicro.publish_speedcmd();
-  
-    xicro.Publisher_PWM.message.vector.x = PWM_leftMotor;
-    xicro.Publisher_PWM.message.vector.y = PWM_rightMotor;
-          
-    xicro.publish_PWM();
-
-    xicro.Publisher_encode.message.vector.x = speed_act_left;
-    xicro.Publisher_encode.message.vector.y = speed_act_right;
-//    xicro.Publisher_encode.message.vector.z = time/1000;
-          
-    xicro.publish_encode();
-
-//
-//   PWM.vector.x = PWM_leftMotor;    //left wheel speed (in m/s)
-//   PWM.vector.y = PWM_rightMotor;   //right wheel speed (in m/s)
-//   PWM_msg.publish(&PWM);
-//   publishSpeed(LOOPTIME);   //Publish odometry on ROS topic
-  
  }
+}
+void publishSpeed(double time) {
+//  xicro.Publisher_encode.message.header.stamp = nh.now;
+  xicro.Publisher_encoder_vel.message.vector.x = speed_act_right;
+  xicro.Publisher_encoder_vel.message.vector.y = speed_act_left;
+  xicro.Publisher_encoder_vel.message.vector.z = time/1000;
+  xicro.publish_encoder_vel();
+  xicro.publish_encoder_tick();
+  xicro.publish_IMU();
+  xicro.Spin_node();
+  xicro.Publisher_speed_req.message.vector.x = speed_req_left;
+  xicro.Publisher_speed_req.message.vector.y = speed_req_right;
+        
+  xicro.publish_speed_req();
+
+    xicro.Publisher_speed_cmd.message.vector.x = speed_cmd_left;
+    xicro.Publisher_speed_cmd.message.vector.y = speed_cmd_right;
+          
+    xicro.publish_speed_cmd();
+  
+    xicro.Publisher_PWM_cmd.message.vector.x = PWM_leftMotor;
+    xicro.Publisher_PWM_cmd.message.vector.y = PWM_rightMotor;
+          
+    xicro.publish_PWM_cmd();
+//  xicro.loginfo("Publishing odometry");
 }
 
 void encoderLeftMotor() {
-  if (digitalRead(PIN_ENCOD_A_MOTOR_LEFT) == digitalRead(PIN_ENCOD_B_MOTOR_LEFT)) pos_left++;
-  else pos_left--;
+  if (digitalRead(PIN_ENCOD_A_MOTOR_LEFT) == digitalRead(PIN_ENCOD_B_MOTOR_LEFT)) pos_left--;
+  else pos_left++;
 }
 
 //Right motor encoder counter
 void encoderRightMotor() {
-  if (digitalRead(PIN_ENCOD_A_MOTOR_RIGHT) == digitalRead(PIN_ENCOD_B_MOTOR_RIGHT)) pos_right--;
-  else pos_right++;
+  if (digitalRead(PIN_ENCOD_A_MOTOR_RIGHT) == digitalRead(PIN_ENCOD_B_MOTOR_RIGHT)) pos_right++;
+  else pos_right--;
 }
 
 template <typename T> int sgn(T val) {
